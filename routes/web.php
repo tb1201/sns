86edc596d49d0f4f038251ba6f216ca3b242c073
y-suzuki <samurai11@kra.biglobe.ne.jp>
2022-12-23 10:13:05 +0000

    5-5 commit

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ArticleController;
 Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
-Route::resource('/articles', ArticleController::class)->except(['index'])->middleware('auth');
+Route::resource('/articles', ArticleController::class)->except(['index, show'])->middleware('auth');
+Route::resource('/articles', ArticleController::class)->only(['show']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
